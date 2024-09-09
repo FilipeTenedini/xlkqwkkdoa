c2c89ccbb3909027b80d1abf06240a8fe0bb5588
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-09 01:29:08 -0300

    feat: update size of first form

--- src/components/sections/banner/Banner.tsx
+++ src/components/sections/banner/Banner.tsx
@@ -19,7 +19,7 @@ const Banner: React.FC = () => {
           </h3>
         </div>
         <form className="w-full md:pl-4 flex justify-center md:justify-start">
-          <div className="w-[50%] gap-2">
+          <div className="w-full md:w-[50%] gap-2">
             <Input placeholder="Seu primeiro nome" className="bg-primary-t-8 border-none rounded-xl mb-2 lg:mb-2"/>
             <Input placeholder="Seu melhor e-mail" className="bg-primary-t-8 border-none rounded-xl mb-2 lg:mb-2" />
             <Button className="w-full md:mb-2">
