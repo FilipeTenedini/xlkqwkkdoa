5c1ade55645443e8efcf9a9728f9337877425df8
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-09 10:26:52 -0300

    feat: update margin of form

--- src/components/sections/banner/Banner.tsx
+++ src/components/sections/banner/Banner.tsx
@@ -3,7 +3,7 @@ import BannerForm from "./BannerForm";
 
 const Banner: React.FC = () => {
   return (
-    <div className="bg-primary-t-6 w-[80%] max-w-5xl rounded-tr-2xl rounded-bl-2xl mt-10 p-4 md:p-0 flex flex-row text-white mb-4">
+    <div className="bg-primary-t-6 w-[80%] max-w-5xl rounded-tr-2xl rounded-bl-2xl mt-4 p-4 md:p-0 flex flex-row text-white mb-4">
       <div className="w-full md:w-[70%] flex flex-col justify-center items-center px-2">
         <span className="text-center font-cinzel border-white border-y text-3xl py-2 px-4 mb-2 sm:mb-5 md:mb-5 md:mt-2 lg:mb-10 md:text-4xl lg:text-5xl">
           Taís Aurich
