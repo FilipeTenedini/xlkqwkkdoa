7b513fab5a400733f64ad26b8da7be5c440604b0
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-10 02:48:56 -0300

    refactor: adjust icon margin

--- src/components/sections/learn/LearnInfos/LearnInfos.tsx
+++ src/components/sections/learn/LearnInfos/LearnInfos.tsx
@@ -10,19 +10,19 @@ const results: {
 }[] = [
   {
     text: "Dicas práticas para otimizar seu tempo de consultório e aumentar o número de pacientes",
-    icon: <CiStopwatch className="text-primary text-xl" />
+    icon: <CiStopwatch className="text-primary text-xl mr-2" />
   },
   {
     text: "Os materiais e técnicas mais modernos no mercado odontológico",
-    icon: <AiOutlineProfile className="text-primary text-xl" />
+    icon: <AiOutlineProfile className="text-primary text-xl mr-2" />
   },
   {
     text: "Como evitar erros comuns e garantir resultados de alta qualidade",
-    icon: <HiOutlineHandThumbDown className="text-primary text-xl" />
+    icon: <HiOutlineHandThumbDown className="text-primary text-xl mr-2" />
   },
   {
     text: "Como essa técnica TRANSFORMOU a minha carreira e pode transformar a sua também!",
-    icon: <FaSortAmountUp className="text-primary text-xl" />,
+    icon: <FaSortAmountUp className="text-primary text-xl mr-2" />,
   }
 ]
 const LearnInfos: React.FC = () => {
