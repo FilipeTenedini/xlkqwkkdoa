882f0bd321011fa1ab7b010512f2d4251c5738a9
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-10 00:19:07 -0300

    feat: improve banner

--- src/components/sections/banner/Banner.tsx
+++ src/components/sections/banner/Banner.tsx
@@ -3,27 +3,27 @@ import BannerForm from "./BannerForm";
 
 const Banner: React.FC = () => {
   return (
-    <div className="bg-primary-t-6 w-[80%] max-w-5xl rounded-tr-2xl rounded-bl-2xl mt-4 p-4 md:p-0 flex flex-row text-white mb-4">
-      <div className="w-full md:w-[70%] flex flex-col justify-center items-center px-2">
+    <div className="bg-primary-t-6 w-[80%] rounded-tr-2xl rounded-bl-2xl mt-4 p-4 md:p-0 flex flex-row text-white mb-4">
+      <div className="w-full md:w-[50%] flex flex-col justify-center items-center px-2">
         <span className="text-center font-cinzel border-white border-y text-3xl py-2 px-4 mb-2 sm:mb-5 md:mb-5 md:mt-2 lg:mb-10 md:text-4xl lg:text-5xl">
           Taís Aurich
         </span>
 
         <div className="mb-2 sm:mb-5 md:mb-5 lg:mb-10">
-          <h2 className="text-center md:text-left md:pl-4 lg:text-xl font-bold">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          <h2 className="text-center md:text-left md:pl-4 lg:text-xl font-bold mb-4">
+            Descubra o Segredo para Transformar Sorrisos e Sua Carreira com Lentes de Resina!
           </h2>
-          <h3 className="text-center md:text-left md:pl-4 lg:text-md">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet consectetur adipisicing elit.
+          <h3 className="text-center md:text-left md:pl-4 lg:text-lg">
+          Participe da minha live exclusiva e aprenda como dominar as técnicas que estão revolucionando o mercado odontológico.
           </h3>
         </div>
         <BannerForm />
       </div>
 
-      <div className="hidden md:flex w-[30%] justify-end">
+      <div className="hidden md:flex w-[50%] justify-end">
         <Image
-          src={"/images/image.webp"}
-          width={375}
+          src={"/images/ban.jpg"}
+          width={855}
           height={500}
           className="rounded-tr-2xl"
           alt="Clinic Dr Banner Image"
--- src/components/sections/banner/BannerForm.tsx
+++ src/components/sections/banner/BannerForm.tsx
@@ -3,12 +3,20 @@ import { Input } from "@/components/ui/Input";
 
 const BannerForm: React.FC = () => {
   return (
-    <form className="w-full md:pl-4 flex justify-center md:justify-start">
-      <div className="w-full md:w-[50%] gap-2">
-        <Input placeholder="Seu primeiro nome" className="bg-primary-t-8 border-none rounded-xl mb-2 lg:mb-2"/>
-        <Input placeholder="Seu melhor e-mail" type="email" className="bg-primary-t-8 border-none rounded-xl mb-2 lg:mb-2" />
-        <Button className="w-full md:mb-2">
-          Lorem Ipsum
+    <form className="w-full md:p-4 flex justify-center">
+      <div className="w-full md:w-[100%] max-w-[700px] gap-2">
+      <Input
+        placeholder="Seu primeiro nome"
+        className="bg-primary-t-4 border-none rounded-xl mb-2 lg:mb-2 shadow-md shadow-primary-t-8 text-center md:h-11"
+      />
+
+        <Input
+          placeholder="Seu melhor e-mail"
+          type="email"
+          className="bg-primary-t-4 border-none rounded-xl mb-2 lg:mb-2 shadow-md shadow-primary-t-8 text-center md:h-11"
+        />
+        <Button className="w-full font-bold md:text-lg md:mb-2 shadow-sm shadow-primary-t-8 md:h-11" variant={"inverted"}>
+          Inscreva-se Agora
         </Button>
       </div>
     </form>
