4888bb316a0b28120fa95124797b736e555ac16a
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-10 01:22:36 -0300

    feat: add learn section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/sections/banner/Banner";
 import LaunchBanner from "@/components/sections/launch-banner/LaunchBanner";
+import Learn from "@/components/sections/learn/Learn";
 import WhoAmI from "@/components/sections/whoami/WhoAmi";
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <Banner />
+      <Learn />
       <WhoAmI />
       <LaunchBanner />
     </div>
