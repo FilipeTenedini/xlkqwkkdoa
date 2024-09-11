00987031b2418d5d004bed9d393f4004345de385
FilipeTenedini <tenedinifilipe@gmail.com>
2024-09-11 14:44:53 -0300

    feat: improve banner date

--- src/components/sections/banner/BannerImage.tsx
+++ src/components/sections/banner/BannerImage.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const BannerImage: React.FC = () => {
   return (
-    <div className="w-full xl:w-[55%] relative h-full">
-      <div className="w-full h-full relative">
+    <div className="relative w-full xl:w-[55%] h-full">
+      <div className="relative w-full h-full">
         <Image
           src={"/images/launch-ban.jpg"}
           width={855}
@@ -11,10 +11,12 @@ const BannerImage: React.FC = () => {
           className="h-full w-full object-cover"
           alt="Clinic Dr Banner Image"
         />
-        <div className="absolute bottom-3 left-6 p-4">
-          <h2 className="text-xl text-white md:text-4xl xl:text-3xl font-bold font-cinzel">
-            03/10 às 20:00
-          </h2>
+        <div className="absolute bottom-10 left-6 right-6">
+          <div className="absolute left-[30%] translate-x-[-50%] -bottom-7 sm:bottom-1 md:bottom-5 lg:bottom-13 xl:bottom-4">
+            <h2 className="text-md text-xl sm:text-2xl lg:text-4xl text-white font-bold font-cinzel p-4">
+              03/10 às 20:00
+            </h2>
+          </div>
         </div>
       </div>
     </div>
