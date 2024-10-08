93e66642de8615131fd29022d19c97d523f3ae36
FilipeTenedini <tenedinifilipe@gmail.com>
2024-10-07 23:43:23 -0300

    v1

--- src/components/curso/components/footer/Footer.tsx
+++ src/components/curso/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Header: React.FC = () => {
   return (
-    <footer className="w-full bg-white h-[30px] text-black text-sm flex items-center pl-6">
+    <footer className="w-full bg-black h-[30px] text-white justify-center md:justify-end text-sm flex items-center md:pr-6">
       2024 © Tais Aurich. Todos os direitos reservados.
     </footer>
   );
