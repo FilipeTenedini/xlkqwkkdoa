8ef3527f04bd0134f8de54df5ecd94cc0a0d5f31
FilipeTenedini <tenedinifilipe@gmail.com>
2024-10-30 12:49:16 -0300

    set new imag

--- src/components/curso/components/about-teacher/AboutTeacher.tsx
+++ src/components/curso/components/about-teacher/AboutTeacher.tsx
@@ -5,15 +5,15 @@ const Header: React.FC = () => {
     <div className="w-full bg-primary flex flex-col items-center justify-center px-3 pt-3 md:px-12 pb-8">
       <div className="w-[230px] lg:mb-4 flex justify-center items-center">
         <img
-          src="/images/new-logo-redonda.png"
-          className="mb-8 w-[180px] h-[150px] lg:h-[200px] lg:w-[320px] ml-[-10px] rounded-full"
+          src="/images/logo-curso.png"
+          className="mb-8 h-[300px] w-auto ml-[-10px] rounded-full"
         />
         <img
           src="/images/image.webp"
           className="rounded-3xl h-[120px] lg:h-[250px]"
         />
       </div>
-      <div className="text-2xl text-white font-semibold mb-6">CONHEÇA A TAIS AURICH</div>
+      <div className="text-2xl text-white font-semibold mb-6 mt-[-70px] md:mt-0">CONHEÇA A TAIS AURICH</div>
       <span className="text-lg text-white lg:max-w-[750px] px-2">
         &nbsp;&nbsp;Formada pela UFRGS, especialista em prótese dentária e com
         mais de 20 anos de experiência.
